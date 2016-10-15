0d0737d3e4fbd65d54857d6cae97b11e01a785ac
tonious <tony.young@capco.com>
2016-10-15 21:11:15 +0100

    cleared original index and created basic hello app

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//strict mode allows improved error handling and disables some less-than-ideal JS language features
+'use strict';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+//loads the react and react-native modules
+var React = require('react');
+var ReactNative = require('react-native');
 
-export default class PropertyFinder extends Component {
+//StyleSheet class used to style the app
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+//PropertyFinderApp extends React.Component, the basic building block of the React UI.
+//Components contain immutable properties, mutable state variables and expose a method for rendering.
+class PropertyFinderApp extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello Rafa Benitez!");
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
+//AppRegistry defines the entry point to the application and provides the root component.
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
